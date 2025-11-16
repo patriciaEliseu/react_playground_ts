65640dc477e97a4288eec2ccdbb0655b23106806
Patricia Eliseu <patyeliseu4@gmail.com>
2025-11-16 17:25:40 -0300

    add Contador e Tarefa

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import './App.css'
-import Home from './pages/home/Home'
+// import Contador from './pages/contador/Contador'
+import Tarefa from './pages/tarefa/Tarefa'
+//import Home from './pages/home/Home'
 
 function App() {
 
   return (
     <>
-        <Home />
-    </>
+        {/* <Home 
+        titulo="Componente Home"
+        texto="Este é o componente Home, que recebe props."
+        /> */}
+         {/* <Contador /> */}
+         
+         <Tarefa />
+
+    </>     
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,14 @@
-﻿
-function Home() {
+﻿interface homeProps {
+  titulo: string;
+  texto: string;
+}
+
+function Home(props: homeProps) {
   return (
-    <div>Componente Home</div>
+    <div>
+      <h2>{props.titulo}</h2>
+      <p>{props.texto}</p>
+    </div>
   )
 }
 
